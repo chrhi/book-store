95d107854fe08c37dc69ebb11ba863448b695799
abdellah <Abdellah.chehri14@gmail.com>
2024-10-26 01:40:22 +0200

    just a small fix

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -12,7 +12,7 @@ const Card: FC<CardProps> = ({ image, price, subTitle, title }) => {
     <div className="w-full h-[600px]   flex flex-col  justify-between">
       <div className="w-full flex flex-col   gap-y-2">
         <img src={image} className="rounded-t-2xl h-[260px] w-full" />
-        <span className="text-gray-500">2006.tammi</span>
+        <span className="text-[#757575]">2006.tammi</span>
       </div>
       <div className="h-[340px] flex flex-col   justify-between gap-y-2   w-full">
         <div className="w-full h-fit my-2">
@@ -21,9 +21,11 @@ const Card: FC<CardProps> = ({ image, price, subTitle, title }) => {
         </div>
 
         <div className="w-full h-fit flex flex-col gap-y-2 my-2  mt-auto">
-          <span>Sidottu</span>
-          <span>Käytetty - erinomainen (K4)</span>
-          <span className="text-3xl font-bold playfair-display">{price}</span>
+          <span className="text-[#757575]">Sidottu</span>
+          <span className="text-[#757575]">Käytetty - erinomainen (K4)</span>
+          <span className="text-3xl font-bold playfair-display text-black">
+            {price}
+          </span>
         </div>
 
         <button className="w-full p-4 bg-[#67BDFF] cursor-pointer hover:bg-[#4ba8f0] rounded-b-2xl font-bold">
