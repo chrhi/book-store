66571cd07efd3031d83fe9eee644f71779f0bf09
abdellah <Abdellah.chehri14@gmail.com>
2024-10-26 00:07:35 +0200

    add the footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -132,64 +132,90 @@ export default function Home() {
 
       <MaxWidthWrapper>
         <div
-          className="w-full h-[425px] p-4"
+          className="w-full h-[425px] p-8 rounded-2xl relative grid grid-cols-2"
           style={{
             backgroundImage: "url('/books-lib.png')",
             backgroundSize: "cover",
             backgroundPosition: "center",
           }}
         >
-          <h2 className="text-white text-[64px]">Salpakirja Oy</h2>
+          <div className=" absolute inset-0 bg-black/35 z-[-1]" />
+          <div className="w-full h-full flex flex-col gap-y-4">
+            <h2 className="text-white text-[64px]">Salpakirja Oy</h2>
 
-          <p className="text-white">
-            Salpakirja Oy on kirjakauppa ja antikvariaatti, jonka kaikki
-            tuotteet löytyvät myös verkkokaupoista www.salpakirja.net ja
-            www.antikvaari.fi. Salpakirjan  Kirjaspotti -nimellä toimivat
-            liikkeet löydät Kotkasta ja Haminasta.
-          </p>
-          <span className="text-white">
-            Vuoden 2024 messukalenteri pitää sisällään noin 150 myyntipäivää,
-            tapahtu mamyynnin aikataulun löydät Messukalenteri välilehdeltä
-            tästä.
-          </span>
-          <p className="text-white">
-            Uusien kirjojen lisäksi löydät liikkeestämme, myös hyväkuntoiset
-            käytetyt kirjat. Antikvariaatti Salpakirjan laajin valikoima löytyy
-            Kirjaspotin Haminan liikkeestä. Antikvariaatti Salpakirjan
-            valikoimaa on esillä vuosittain myös Helsingin –, Jyväskylän ja
-            Turun kirjamessuilla sekä muutamissa pienemmissä tapahtumissa.
-          </p>
+            <p className="text-white">
+              Salpakirja Oy on kirjakauppa ja antikvariaatti, jonka kaikki
+              tuotteet löytyvät myös verkkokaupoista www.salpakirja.net ja
+              www.antikvaari.fi. Salpakirjan  Kirjaspotti -nimellä toimivat
+              liikkeet löydät Kotkasta ja Haminasta.
+            </p>
+            <span className="text-white">
+              Vuoden 2024 messukalenteri pitää sisällään noin 150 myyntipäivää,
+              tapahtu mamyynnin aikataulun löydät Messukalenteri välilehdeltä
+              tästä.
+            </span>
+            <p className="text-white">
+              Uusien kirjojen lisäksi löydät liikkeestämme, myös hyväkuntoiset
+              käytetyt kirjat. Antikvariaatti Salpakirjan laajin valikoima
+              löytyy Kirjaspotin Haminan liikkeestä. Antikvariaatti Salpakirjan
+              valikoimaa on esillä vuosittain myös Helsingin –, Jyväskylän ja
+              Turun kirjamessuilla sekä muutamissa pienemmissä tapahtumissa.
+            </p>
+          </div>
         </div>
       </MaxWidthWrapper>
+      <div className="w-full h-fit my-8">
+        <div className="w-full h-[11px] bg-[#67BDFF]"></div>
+        <div className="w-full bg-[#F5F5F5] pt-4">
+          <MaxWidthWrapper>
+            <div className="bg-[#F5F5F5] text-[#757575] w-full h-[478px] grid grid-cols-2">
+              <div className="w-full h-full flex flex-col gap-y-4">
+                <span className="text-[#757575] text-2xl">Salpakirja Oy</span>
+
+                <span>Salpakirja Oy © 2024</span>
+                <span>
+                  Tietosuojaseloste 
+                  <br />
+                  Evästeet
+                </span>
+              </div>
+
+              <div className="w-full h-full">
+                <span className="text-[#757575]">Yhteystiedot:</span>
+                <span className="text-[#757575]">
+                  Salpakirja Oy asiakaspalvelu
+                  <br />
+                  salpakirja@gmail.com tai puh. 050 339 5724
+                </span>
+                <span className="text-[#757575]">
+                  Kirjaspotti Hamina
+                  <br />
+                  Avoinna ma, ke-pe klo 9-17, ti klo 8-16
+                  <br />
+                  Raatihuoneentori 16, 49400 Hamina
+                  <br />
+                  salpakirja@gmail.com, puh. 050 339 5724
+                </span>
+                <span className="text-[#757575]">
+                  Kirjaspotti Kotka
+                  <br />
+                  Avoinna ma-pe klo 10.15-18, la klo 10.15-16, su suljettu.
+                  <br />
+                  Keskuskatu 10, 48100 Kotka, Kauppakeskus Pasaati katutaso
+                  <br />
+                  kirjaspotti.kotka@gmail.com, puh. 041 314 7501
+                </span>{" "}
+                */
+              </div>
+            </div>
+          </MaxWidthWrapper>
+        </div>
+      </div>
     </>
   );
 }
 
 {
-  /* <span className="text-[#757575]">Yhteystiedot:</span>
-<span className="text-[#757575]">
-  Salpakirja Oy asiakaspalvelu
-  <br />
-  salpakirja@gmail.com tai puh. 050 339 5724
-</span>
-<span className="text-[#757575]">
-  Kirjaspotti Hamina
-  <br />
-  Avoinna ma, ke-pe klo 9-17, ti klo 8-16
-  <br />
-  Raatihuoneentori 16, 49400 Hamina
-  <br />
-  salpakirja@gmail.com, puh. 050 339 5724
-</span>
-<span className="text-[#757575]">
-  Kirjaspotti Kotka
-  <br />
-  Avoinna ma-pe klo 10.15-18, la klo 10.15-16, su suljettu.
-  <br />
-  Keskuskatu 10, 48100 Kotka, Kauppakeskus Pasaati katutaso
-  <br />
-  kirjaspotti.kotka@gmail.com, puh. 041 314 7501
-</span> */
 }
 
 {
